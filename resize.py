0310515be8a14df89737bd9e3cb0916c557776b8
Rohan Khandekar <rohan1198@gmail.com>
2021-12-06 12:58:37 +0530

    Added multiprocessing and logging

--- resize.py
+++ resize.py
@@ -1,52 +1,107 @@
 import os
-import cv2
 import argparse
+import numpy as np
+import cv2
+import logging
+import time
 from distutils.dir_util import copy_tree
-from tqdm import tqdm
+from imutils import paths
+from multiprocessing import Pool, cpu_count
+from functools import partial
 
 
 
-def resize(path, size = 416):
-    """
-    Simple function to resize the image to the specified size
-    input -> image path
-    output -> array of resized image
-    """
-    im = cv2.imread(path)
-    img = cv2.resize(im, (size, size))
 
-    return img
+def resize_image(image: str, size: int = 416):
+    resized = cv2.resize(image, (size, size))
+    return resized
+
+
+def make_batch(imgs_list: list, batch_size: int):
+    for i in range(0, len(imgs_list), batch_size):
+        yield imgs_list[i: i + batch_size]
+
+
+def process_batch(payload: list, size: int):
+        logger.info(f"starting process {payload['id']}")
+
+        for imagePath in payload["input_paths"]:
+            image = cv2.imread(imagePath)
+            resized_image = resize_image(image, size)
+            cv2.imwrite(imagePath, resized_image)
+
+
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "Resize all images in all folders, retaining the structure")
+    parser = argparse.ArgumentParser(description = "Resize images, retaining folder structure")
+    parser.add_argument("--dir", required = True, type = str, help = "Path to the dataset")
+    parser.add_argument("--size", type = int, default = 416, help = "Size to resize the image to")
+    parser.add_argument("--num-procs", type = int, default = cpu_count()-1, help = f"Number of threads to run in parallel. Keep less than {cpu_count()}")
 
-    parser.add_argument("--dir", type = str, required = True, help = "Path to the input image directory")
-    parser.add_argument("--size", type = int, default = 416, help = "Size of the resized image")
-    
     args = parser.parse_args()
+    
+    logging.basicConfig(filename = "resize.log", filemode = "w", format = '%(asctime)s | %(name)s | %(message)s', datefmt = '%Y-%m-%d  %H:%M:%S')
+    logger = logging.getLogger('Resize Dataset')
+    logger.setLevel(logging.INFO)
+    
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(message)s', datefmt = '%Y-%m-%d::%H:%M:%S')
+    ch.setFormatter(formatter)
+    
+    logger.addHandler(ch)
+
+
+    start_time = time.perf_counter()
 
     # Check if directory exists
-    if not os.path.isdir("./resized_dataset"):
-        os.mkdir("./resized_dataset")
-        print("Created ./resized_dataset directory")
-
-    # If the directory is empty, copy the files from the original dataset, else skip
-    if  len(os.listdir("./resized_dataset")) == 0:
-        copy_tree(args.dir, "./resized_dataset")
-        print("Successfully copied all files to the resized_dataset folder")
+    if not os.path.exists("./temp"):
+        os.mkdir("./temp")
+        logger.info("Created temp folder to store images")
     else:
-        print("Directory is not empty")
+        logger.info("temp folder already exists.")
+
+    # If directory, is not empty, copy files from the original dataset
+    if len(os.listdir("./temp")) == 0:
+        logger.info("Copying files from original dataset folder...")
+        copy_tree(args.dir, "./temp")
+        logger.info("All files successfully copied.")
+    else:
+        logger.info("Directory is not empty. Skipping...")
         pass
 
-    # Resize image
-    print("Resizing images...")
-    for root, dirs, files in os.walk("./resized_dataset"):
-        for name in tqdm(files):
-            filepath = root + os.sep + name
+    processes = args.num_procs if args.num_procs > 0 else cpu_count()
+    process_id = list(range(0, processes))
 
-            if filepath.endswith(".jpg"):
-                resized_img = resize(path = os.path.join(root, name), size = args.size)
-                cv2.imwrite(os.path.join(root, name), resized_img)
-    print("Datset resized!")
+    logger.info("Grabbing image...")
+    
+    path_images = sorted(list(paths.list_images("./temp")))
+    num_images_per_process = int(np.ceil(len(path_images) / float(processes)))
+
+    logger.info(f"Resizing {int(len(path_images))} images...")
+    logger.info(f"Processing images in batches of {num_images_per_process}")
+
+    chunked_paths = list(make_batch(path_images, num_images_per_process))
+
+    payloads = []
+
+    for i, image_paths in enumerate(chunked_paths):
+        data = {"id": i, "input_paths": image_paths}
+        payloads.append(data)
+
+    logger.info(f"Launching {processes} processes...")
+    pool = Pool(processes = processes)
+    pool.map(partial(process_batch, size = args.size), payloads)
+
+    logger.info("Processing...")
+    pool.close()
+    pool.join()
+    logger.info("Dataset Resized!")
+
+    end_time = time.perf_counter()
+
+    logger.info("=" * 40)
+    logger.info(f"Time taken: {end_time - start_time} seconds")
+    logger.info("=" * 40)
